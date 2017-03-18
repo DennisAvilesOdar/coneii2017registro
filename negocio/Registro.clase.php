601583f4ce5fba81f7c389e2db9299b668caeaec
Dennis Aviles <dennis.aviles@icloud.com>
2017-03-18 01:36:11 -0500

    .

--- negocio/Registro.clase.php
+++ negocio/Registro.clase.php
@@ -1,8 +1,47 @@
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+require_once '../datos/Conexion.clase.php';
+date_default_timezone_set("America/Lima");
 
+class Registro extends Conexion {
+    private $dni;
+    private $fecha_vencimiento;
+    private $monto;
+    
+    function getDni() {
+        return $this->dni;
+    }
+
+    function getFecha_vencimiento() {
+        return $this->fecha_vencimiento;
+    }
+
+    function getMonto() {
+        return $this->monto;
+    }
+
+    function setDni($dni) {
+        $this->dni = $dni;
+    }
+
+    function setFecha_vencimiento($fecha_vencimiento) {
+        $this->fecha_vencimiento = $fecha_vencimiento;
+    }
+
+    function setMonto($monto) {
+        $this->monto = $monto;
+    }
+    
+    public function cargarTabla() { //obtener toda la tabla
+        try {
+            $sql = "select * from registrados_pendientes";
+            $sentencia = $this->dblink->prepare($sql);
+            $sentencia->execute();
+            $resultado = $sentencia->fetchAll(PDO::FETCH_ASSOC);
+            return $resultado;
+        } catch (Exception $exc) {
+            throw $exc;
+        }
+    }
+    
+}
